dec7e76095c1fd910fe868dae2aea4f890e88993
Stephen Gregory <stephengregorynl@gmail.com>
2018-02-06 22:48:23 -0330

    Add tests for onmessage

--- test/index.js
+++ test/index.js
@@ -1,7 +1,62 @@
 const test = require('tape');
 const fn = require('..');
+const MockServer = require('mock-socket').Server;
 
 test('exports', t => {
 	t.is(typeof fn, 'function', 'exports a function');
 	t.end();
+});
+
+test('onmessage', t => {
+	t.test('should receive data as string when server sends string', st => {
+		st.timeoutAfter(500);
+		const mockServer = new MockServer('ws://localhost:8080');
+
+		const stringFromServer = 'foo bar biz';
+
+		const ws = new fn('ws://localhost:8080', {
+			onmessage: e => {
+				mockServer.stop();
+				st.equal(typeof e.data, 'string', 'Event data is a string');
+				st.deepEqual(e.data, stringFromServer, 'Event data is the same as the server data');
+				st.end();
+			}
+		});
+
+		mockServer.send(stringFromServer);
+	});
+
+	t.test('should receive data as object when server sends object', st => {
+		const mockServer = new MockServer('ws://localhost:8080');
+		st.timeoutAfter(500);
+		const jsonMessageFromServer = { foo: "bar", baz: [1, 2, 3] };
+
+		mockServer.start();
+		const ws = new fn('ws://localhost:8080', {
+			onmessage: e => {
+				mockServer.stop();
+				st.equal(typeof e.data, 'object', 'Event data is an object');
+				st.deepEqual(e.data, jsonMessageFromServer, 'Event data is the same as the server data');
+				st.end();
+			}
+		});
+		mockServer.send(JSON.stringify(jsonMessageFromServer));
+	});
+
+	t.test('should receive data as ArrayBuffer when server sends ArrayBuffer', st => {
+		const mockServer = new MockServer('ws://localhost:8080');
+		st.timeoutAfter(500);
+		const arrayBufferFromServer = new ArrayBuffer(42);
+
+		mockServer.start();
+		const ws = new fn('ws://localhost:8080', {
+			onmessage: e => {
+				mockServer.stop();
+				st.equal(typeof e.data, 'object', 'Event data is an object');
+				st.deepEqual(e.data, arrayBufferFromServer, 'Event data is the same as the server data');
+				st.end();
+			}
+		});
+		mockServer.send(JSON.stringify(arrayBufferFromServer));
+	});
 });
