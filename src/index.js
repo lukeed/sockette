0a9c3399bcfa1528de15d681afc55ebeaa5c6b54
Luke Edwards <luke.edwards05@gmail.com>
2018-02-12 10:38:30 -0800

    breaking: fix transmits after reconnecting;
    - requires the `new` syntax

--- src/index.js
+++ src/index.js
@@ -3,40 +3,49 @@ function noop() {}
 export default function (url, opts) {
 	opts = opts || {};
 
-	let k, ws, num, $={};
+	let k, ws, num, $={}, self=this;
 	let ms=opts.timeout || 1e3, max=opts.maxAttempts || Infinity;
 
 	$.onmessage = opts.onmessage || noop;
 
 	$.onclose = e => {
-		(e.code !== 1e3 && e.code !== 1005) && $.reconnect(e);
+		(e.code !== 1e3 && e.code !== 1005) && self.reconnect(e);
 		(opts.onclose || noop)(e);
 	};
 
 	$.onerror = e => {
-		(e && e.code==='ECONNREFUSED') ? $.reconnect(e) : (opts.onerror || noop)(e);
+		(e && e.code==='ECONNREFUSED') ? self.reconnect(e) : (opts.onerror || noop)(e);
 	};
 
 	$.onopen = e => {
 		num=0; (opts.onopen || noop)(e);
 	};
 
-	$.open = () => {
+	self.open = () => {
 		ws = new WebSocket(url, opts.protocols);
 		for (k in $) ws[k] = $[k];
-		return ws;
 	};
 
-	$.reconnect = e => {
+	self.reconnect = e => {
 		(++num < max) && setTimeout(_ => {
 			(opts.onreconnect || noop)(e);
-			$.open();
+			self.open();
 		}, ms);
 	};
 
-	$.json = x => {
+	self.json = x => {
 		ws.send(JSON.stringify(x));
 	};
 
-	return $.open();
+	self.send = x => {
+		ws.send(x);
+	};
+
+	self.close = (x, y) => {
+		ws.close(x, y);
+	};
+
+	self.open(); // init
+
+	return self;
 }
