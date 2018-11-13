8d0bcd666c8ecb708bef0d5b9ad73e7297ab58fd
Luke Edwards <luke.edwards05@gmail.com>
2018-11-13 15:44:51 -0800

    refactor: inline all ws.* methods; saves 18 bytes!

--- src/index.js
+++ src/index.js
@@ -3,50 +3,49 @@ function noop() {}
 export default function (url, opts) {
 	opts = opts || {};
 
-	var k, ws, num=0, $={}, self=this;
+	var ws, num=0, $={};
 	var ms=opts.timeout || 1e3, max=opts.maxAttempts || Infinity;
 
-	$.onmessage = opts.onmessage || noop;
-
-	$.onclose = function (e) {
-		e.code === 1e3 || e.code === 1005 || self.reconnect(e);
-		(opts.onclose || noop)(e);
-	};
-
-	$.onerror = function (e) {
-		(e && e.code==='ECONNREFUSED') ? self.reconnect(e) : (opts.onerror || noop)(e);
-	};
-
-	$.onopen = function (e) {
-		num = 0;
-		(opts.onopen || noop)(e);
-	};
-
-	self.open = function () {
+	$.open = function () {
 		ws = new WebSocket(url, opts.protocols || []);
-		for (k in $) ws[k] = $[k];
+
+		ws.onmessage = opts.onmessage || noop;
+
+		ws.onopen = function (e) {
+			(opts.onopen || noop)(e);
+			num = 0;
+		};
+
+		ws.onclose = function (e) {
+			e.code === 1e3 || e.code === 1005 || $.reconnect(e);
+			(opts.onclose || noop)(e);
+		};
+
+		ws.onerror = function (e) {
+			(e && e.code==='ECONNREFUSED') ? $.reconnect(e) : (opts.onerror || noop)(e);
+		};
 	};
 
-	self.reconnect = function (e) {
+	$.reconnect = function (e) {
 		(num++ < max) ? setTimeout(_ => {
 			(opts.onreconnect || noop)(e);
-			self.open();
+			$.open();
 		}, ms) : (opts.onmaximum || noop)(e);
 	};
 
-	self.json = function (x) {
+	$.json = function (x) {
 		ws.send(JSON.stringify(x));
 	};
 
-	self.send = function (x) {
+	$.send = function (x) {
 		ws.send(x);
 	};
 
-	self.close = function (x, y) {
+	$.close = function (x, y) {
 		ws.close(x, y);
 	};
 
-	self.open(); // init
+	$.open(); // init
 
-	return self;
+	return $;
 }
