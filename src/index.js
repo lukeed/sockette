0d509bfafe0b796920efef29532d26a351ed6050
Gabriel Araújo <gabrieldea.silva@gmail.com>
2020-04-23 14:54:16 -0300

    Add promise to init socket

--- src/index.js
+++ src/index.js
@@ -1,29 +1,33 @@
-function noop() {}
+function noop() { }
 
 export default function (url, opts) {
 	opts = opts || {};
 
-	var ws, num=0, timer=1, $={};
+	var ws, num = 0, timer = 1, $ = {};
 	var max = opts.maxAttempts || Infinity;
 
 	$.open = function () {
-		ws = new WebSocket(url, opts.protocols || []);
+		return new Promise(function (resolve, reject) {
+			ws = new WebSocket(url, opts.protocols || []);
 
-		ws.onmessage = opts.onmessage || noop;
+			ws.onmessage = opts.onmessage || noop;
 
-		ws.onopen = function (e) {
-			(opts.onopen || noop)(e);
-			num = 0;
-		};
+			ws.onopen = function (e) {
+				(opts.onopen || noop)(e);
+				num = 0;
+				resolve($)
+			};
 
-		ws.onclose = function (e) {
-			e.code === 1e3 || e.code === 1001 || e.code === 1005 || $.reconnect(e);
-			(opts.onclose || noop)(e);
-		};
+			ws.onclose = function (e) {
+				e.code === 1e3 || e.code === 1001 || e.code === 1005 || $.reconnect(e);
+				(opts.onclose || noop)(e);
+			};
 
-		ws.onerror = function (e) {
-			(e && e.code==='ECONNREFUSED') ? $.reconnect(e) : (opts.onerror || noop)(e);
-		};
+			ws.onerror = function (e) {
+				(e && e.code === 'ECONNREFUSED') ? $.reconnect(e) : (opts.onerror || noop)(e);
+				reject(e)
+			};
+		})
 	};
 
 	$.reconnect = function (e) {
@@ -50,7 +54,5 @@ export default function (url, opts) {
 		ws.close(x || 1e3, y);
 	};
 
-	$.open(); // init
-
-	return $;
+	return $.open();
 }
